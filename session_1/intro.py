c4925ea251e1fb68c6c3cfe06e16d3e1bc4ee4b0
Gustavo Sotres <gus.sotres@gmail.com>
2023-01-16 21:25:09 -0600

    Sesion1

---
+++ session_1/intro.py
@@ -0,0 +1,26 @@
+
+class Cat:
+	"""A class to represent a Cat
+	"""
+
+	def __init__(self, name : str, age: int):
+		"""A new cat will be created
+		"""
+		self.name = name
+		self.age = age
+
+	def __repr__(self) -> str:
+		return f"The cat: {self.name}, is {self.age} years old."
+
+
+def main():
+
+	tom = Cat("Tom", 3)
+	luna = Cat("Luna", 8)
+
+	print(tom)
+	print(luna)
+
+
+if __name__ == "__main__":
+	main()
