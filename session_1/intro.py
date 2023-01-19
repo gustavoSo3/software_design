aba759a983c38226dda05d25c3f2d874471bf1ae
Gustavo Sotres <gus.sotres@gmail.com>
2023-01-19 07:30:30 -0600

    Methods

--- session_1/intro.py
+++ session_1/intro.py
@@ -1,36 +1,49 @@
 
 class Cat:
-	"""A class to represent a Cat
-	"""
+    """A class to represent a Cat
+    """
 
-	def __init__(self, name : str, age: int):
-		"""A new cat will be created
-		"""
-		self.name = name
-		self.age = age
-		self.accesories = [
-			"Hello there",
-			3.1415,
-			{
-				"enemy": "dog"
-			},
-			[
-				"NICE",
-			],
-		]
+    def __init__(self, name: str, age: int):
+        """A new cat will be created
+        """
+        self.name = name
+        self.age = age
+        self.accesories = [
+            "Hello there",
+            3.1415,
+            {
+                "enemy": "dog"
+            },
+            [
+                "NICE",
+            ],
+        ]
+        self.vacunado = False
 
-	def __repr__(self) -> str:
-		return f"The cat: {self.name}, is {self.age} years old."
+    def vacunar(self) -> bool:
+        """Que crazy te van a picar
+
+        Valiste
+        """
+        self.vacunado = True
+        return self.vacunado
+
+    def __repr__(self) -> str:
+        return f"The cat: {self.name}, is {self.age} years old."
 
 
 def main():
 
-	tom = Cat("Tom", 3)
-	luna = Cat("Luna", 8)
+    tom = Cat("Tom", 3)
+    luna = Cat("Luna", 8)
 
-	print(tom)
-	print(luna)
+    print(tom)
+    print(luna)
+
+    print("Vacunando")
+    tom.vacunar()
+    luna.vacunar()
 
 
 if __name__ == "__main__":
-	main()
+    main()
